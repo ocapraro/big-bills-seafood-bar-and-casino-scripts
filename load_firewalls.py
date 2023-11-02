811ce57758492051cd646a82fee00553f376e009
Oscar Capraro <oscar@mcapraro.com>
2023-11-01 23:11:18 -0400

    setup basic menu

--- load_firewalls.py
+++ load_firewalls.py
@@ -6,11 +6,12 @@ def success(output):
 
 def install():
   # Install ufw
+  print("Initializing ufw")
   for i in "12345":
     ufw_installed = os.popen("sudo ufw status").read()
     if success(ufw_installed):
       return True
-    os.system("clear")
+    display_header()
     input("ufw not found, retrying")
     install_ufw = os.popen("sudo apt-get ufw").read()
     if not success(install_ufw):
@@ -27,13 +28,38 @@ def enable_defaults():
   success(os.popen("sudo ufw default deny incoming").read()) and \
   success(os.popen("sudo ufw default allow outgoing").read())
 
+def title(text):
+  print("="*(32+len(text)))
+  print("="*15,"\033[32m"+text+"\033[0m","="*15)
+  print("="*(32+len(text)))
+
+def display_header():
+  os.system("clear")
+  title("Linux Firewall Setup")
+
+def display_menu():
+  display_header()
+  print("""
+  0. Exit
+  1. Initialize ufw
+""")
 
 def main():
-  code = True
-  if code:
-    code = install()
-    if code:
-      code = enable_defaults()
+  while 1:
+    display_menu()
+    try:
+      choice = int(input("Select an option: "))
+    except ValueError:
+      continue
+    if choice == 0:
+      break
+    elif choice == 1:
+      code = True
+      if code:
+        code = install()
+        if code:
+          code = enable_defaults()
+  print("Successfully Exited")
   
   
 
