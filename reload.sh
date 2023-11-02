cafcfd8c82318f31fdddc250664b7801544e74d4
Oscar Capraro <oscar@mcapraro.com>
2023-11-01 23:22:07 -0400

    add reload script

---
+++ reload.sh
@@ -0,0 +1,3 @@
+rm -rf big-bills-seafood-bar-and-casino-scripts
+git clone https://github.com/ocapraro/big-bills-seafood-bar-and-casino-scripts
+python3 ./big-bills-seafood-bar-and-casino-scripts/load_firewalls.py
